f38d80b18ba7ef15fd06103da4a07cd0796a2556
Robbert van Mourik <robbert@webstack.nl>
2019-10-31 12:54:04 +0100

    Initial setup

---
+++ WebstackApiPlatformGlobalSearchBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webstack\ApiPlatformGlobalSearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebstackApiPlatformGlobalSearchBundle extends Bundle
+{
+
+}
