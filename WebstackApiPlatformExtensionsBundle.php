2e17176c5b180c53cb48707b1b76a70963103f68
Robbert van Mourik <robbert@webstack.nl>
2019-12-12 11:50:03 +0100

    Removed obsolete use

--- WebstackApiPlatformExtensionsBundle.php
+++ WebstackApiPlatformExtensionsBundle.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Webstack\ApiPlatformExtensionsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Webstack\ApiPlatformExtensionsBundle\DependencyInjection\Compiler\MePass;
-
 
 /**
  * Class WebstackApiPlatformExtensionsBundle
