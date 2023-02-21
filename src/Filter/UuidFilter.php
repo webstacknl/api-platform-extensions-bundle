c31d3f60897e20c439e5d2f07c12f6faa6878f0c
Jurgo Touw <jurgo@webstack.nl>
2023-02-21 10:48:50 +0100

    feature: add doctrine-bundle

--- src/Filter/UuidFilter.php
+++ src/Filter/UuidFilter.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Webstack\ApiPlatformExtensionsBundle\Filter;
 
-use Doctrine\DBAL\Types\Type;
 use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
